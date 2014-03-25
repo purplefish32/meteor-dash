7b382aebfe9fb82691daf253e2883a2bf44f22cb
Donovan Tengblad <purplefish32@gmail.com>
2014-03-25 14:54:43 +0100

    Working ping

--- testmeteor.js
+++ testmeteor.js
@@ -1,10 +1,10 @@
 Router.map(function(){
-    this.route('dashboard', {path: '/'});
-    this.route('hosts');
-    this.route('reports');
-    this.route('admin');
-    this.route('login');
-    this.route('logout');
+  this.route('dashboard', {path: '/'});
+  this.route('hosts');
+  this.route('reports');
+  this.route('admin');
+  this.route('login');
+  this.route('logout');
 });
 
 
@@ -13,22 +13,22 @@ chatStream = new Meteor.Stream('chat');
 if (Meteor.isClient) {
   chatStream.on('message', function(message) {
     Messenger().post(message);
-  });
+});
 
   Template.login.events({
     'submit #login-form' : function(e, t){
       e.preventDefault();
       // retrieve the input field values
       var email = t.find('#login-email').value
-        , password = t.find('#login-password').value;
+      , password = t.find('#login-password').value;
 
         // Trim and validate your fields here....
 
         // If validation passes, supply the appropriate fields to the
         // Meteor.loginWithPassword() function.
         Meteor.loginWithPassword(email, password, function(err){
-        if (err) {
-          console.log(err);
+          if (err) {
+            console.log(err);
         }
           // The user might not have been found, or their passwword
           // could be incorrect. Inform the user that their
@@ -36,40 +36,56 @@ if (Meteor.isClient) {
         //else
           // The user has been logged in.
       });
-         return false;
-      }
-  });
+        return false;
+    }
+});
 }
 
 if (Meteor.isServer) {
-  chatStream.permissions.read(function() {
-    return true;
-  });
+    /*chatStream.permissions.read(function() {
+        return true;
+    });
 
-  chatStream.permissions.write(function() {
-    return true;
-  });
-
-
-  Meteor.startup(function () {
-    // code to run on server at startup
+    chatStream.permissions.write(function() {
+        return true;
+    });*/
 
 
-    setInterval(function() {
-      msg = getMessage();
-        Message = {
-        message: msg,
-        hideAfter: 7
-  };
-  chatStream.emit('message', msg);
-    }, Math.random()*10000);
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+
+        Meteor.methods({
+            getData: function() {
+                var url = 'http://88.80.186.29/linux-dash/sh/mem.php';
+                var result = Meteor.http.get(url, {timeout:30000});
+                if(result.statusCode == 200) {
+                    var respJson = JSON.parse(result.content);
+                    console.log("response received.");
+                    //custom code
+                    if(result.content) { //IF OK
+                        console.log('returning response');
+                        return respJson;
+                    } else {
+                        throw new Meteor.Error('error', 'error-text');
+                    }
+                } else {
+                    console.log("Content issue: ", result.statusCode);
+                    throw new Meteor.Error("");
+                }
+            }
+        });
+
+        Meteor.setInterval(function() {
+            data = Meteor.call('getData');
+
+            console.log(data[1]);
+
+            Message = {
+                message: data,
+                hideAfter: 7
+            };
+
+            chatStream.emit('message', data[1]);
+        }, Math.random()*1000);
+    });
 }
-
-getMessage = function() {
-  HTTP.call("GET", "http://www.iheartquotes.com/api/v1/random", function(err, result) {
-    if (!err)
-      console.log(result.content);
-      return result.content;
-  });
-};
